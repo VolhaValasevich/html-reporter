3ff18efe03bb97678a6ba1683e91d37f8d839d3b
Volha Valasevich <volha_valasevich@epam.com>
2018-08-23 05:21:28 +0300

    move html header and css styles into a separate file

--- index.js
+++ index.js
@@ -1,73 +1,7 @@
 const fs = require("fs");
 const yargs = require("yargs").argv;
 const path = yargs.path;
-
-const reportHeader = `<!DOCTYPE html>
-<html>
-  <head>
-    <title>Cucumber Feature Report</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style type="text/css">
-     body {
-    font: 16px Calibri, Courier, monospace;
-}
-h1, h2, h3, h4, p {
-    margin: 0;
-    padding: 0;
-}
-.highlight {
-    color: #111111;
-    font-weight: bold;
-}
-.failed {
-    background-color: #ff9999;
-}
-.passed {
-    background-color: #99ff99;
-}
-.skipped {
-	background-color: #ffff80;
-}
-.float-right {
-    float: right;
-}
-.header {
-    background-color: #d9d9d9;
-    font: 20px Arial, Courier, monospace;
-    border-radius: 5px;
-    padding: 20px;
-}
-
-.header-text {
-    font: 50px Calibri, Courier, monospace;
-}
-.step {
-    margin: 10px;
-}
-.step .text {
-    border-radius: 3px;
-    color: #666666;
-    padding: 5px;
-    display: block;
-}
-.block {
-    background: #f8f8f8;
-    border-radius: 5px;
-    border: 1px solid #e8e8e8;
-    padding: 10px;
-    margin: 10px;
-}
-.screenshot {
-    margin: 8px;
-    width: 30%;
-}
-    </style>
-    <meta charset="UTF-8">
-  </head>
-  <body>`;
+const reportHeader = require("./data/htmlStyles.js")
 
 const reportEnd = `</body></html>`;
 
